5131ed2d0f79e6a4d399bbb9bd24fb714dbfaba7
Carlos Castillo Ruiz <carlos_2110@outlook.com>
2026-08-23 11:30:05 -0500

    Inicio del proyecto

---
+++ src/main/java/com/utp/reportes_ciudadanos/ReportesCiudadanosApplication.java
@@ -0,0 +1,13 @@
+package com.utp.reportes_ciudadanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportesCiudadanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportesCiudadanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/reportes_ciudadanos/ReportesCiudadanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.reportes_ciudadanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportesCiudadanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
